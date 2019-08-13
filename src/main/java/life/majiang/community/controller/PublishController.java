1fef8bdd36cb140077dedc6131b750b0c38057dc
Xu Wang <xuxu.wang@mail.utoronto.ca>
2019-08-13 13:37:11 +0800

    just commit

--- src/main/java/life/majiang/community/controller/PublishController.java
+++ src/main/java/life/majiang/community/controller/PublishController.java
@@ -1,13 +1,53 @@
 package life.majiang.community.controller;
 
+import life.majiang.community.mapper.QuestionMapper;
+import life.majiang.community.mapper.UserMapper;
+import life.majiang.community.model.Question;
+import life.majiang.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class PublishController {
 
+    @Autowired
+    private QuestionMapper questionMapper;
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/publish")
     public String publish(){
+        return "publish";
+    }
+
+    @PostMapping
+    public String doPublish(@RequestParam("title") String title,
+                            @RequestParam("description") String description,
+                            @RequestParam("tag") String tag,
+                            HttpServletRequest request){
+        Question question = new Question();
+        question.setTitle(title);
+        question.setDescription(description);
+        question.setTag(tag);
+        Cookie[] cookies = request.getCookies();
+        for(Cookie cookie:cookies){
+            if(cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if(user!=null){
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
+        questionMapper.create(question);
         return "publish";
     }
 }
