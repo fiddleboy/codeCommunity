566534aaccbb820bec581f40e0382f2f3926dea9
Xu Wang <xuxu.wang@mail.utoronto.ca>
2019-08-13 14:52:44 +0800

    bootstrap not working

--- src/main/java/life/majiang/community/controller/PublishController.java
+++ src/main/java/life/majiang/community/controller/PublishController.java
@@ -6,6 +6,7 @@ import life.majiang.community.model.Question;
 import life.majiang.community.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,23 +32,35 @@ public class PublishController {
     public String doPublish(@RequestParam("title") String title,
                             @RequestParam("description") String description,
                             @RequestParam("tag") String tag,
+                            Model model,
                             HttpServletRequest request){
-        Question question = new Question();
-        question.setTitle(title);
-        question.setDescription(description);
-        question.setTag(tag);
+
+        User user = null;
         Cookie[] cookies = request.getCookies();
         for(Cookie cookie:cookies){
             if(cookie.getName().equals("token")){
                 String token = cookie.getValue();
-                User user = userMapper.findByToken(token);
+                user = userMapper.findByToken(token);
                 if(user!=null){
                     request.getSession().setAttribute("user", user);
                 }
                 break;
             }
         }
+
+        if(user == null){
+            model.addAttribute("error", "用户未登录");
+            return "publish";
+        }
+
+        Question question = new Question();
+        question.setTitle(title);
+        question.setDescription(description);
+        question.setTag(tag);
+        question.setCreator(user.getId());
+        question.setGmtCreate(System.currentTimeMillis());
+        question.setGmtModified(question.getGmtCreate());
         questionMapper.create(question);
-        return "publish";
+        return "redirect:/";
     }
 }
