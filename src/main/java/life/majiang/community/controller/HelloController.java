6c99b5562e367f8b13faf91582651b496a83d5d7
Xu Wang <xuxu.wang@mail.utoronto.ca>
2019-08-15 09:52:01 +0800

    The front page(index page) has been added with new features, no bugs until this point.

--- src/main/java/life/majiang/community/controller/HelloController.java
+++ src/main/java/life/majiang/community/controller/HelloController.java
@@ -1,13 +1,19 @@
 package life.majiang.community.controller;
 
+import life.majiang.community.dto.QuestionDTO;
+import life.majiang.community.mapper.QuestionMapper;
 import life.majiang.community.mapper.UserMapper;
+import life.majiang.community.model.Question;
 import life.majiang.community.model.User;
+import life.majiang.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 
 @Controller
@@ -16,20 +22,29 @@ public class HelloController {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private QuestionService questionService;
+
     // '/' stands for root directory
     @GetMapping("/")
-    public String index(HttpServletRequest request){
+    public String index(HttpServletRequest request,
+                        Model model) {
         Cookie[] cookies = request.getCookies();
-        for(Cookie cookie:cookies){
-            if(cookie.getName().equals("token")){
-                String token = cookie.getValue();
-                User user = userMapper.findByToken(token);
-                if(user!=null){
-                    request.getSession().setAttribute("user", user);
+        if (cookies != null && cookies.length != 0) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
                 }
-                break;
             }
         }
+
+        List<QuestionDTO> questionList = questionService.list();
+        model.addAttribute("questions", questionList);
         return "index";
     }
 
--- src/main/java/life/majiang/community/dto/QuestionDTO.java
+++ src/main/java/life/majiang/community/dto/QuestionDTO.java
@@ -1,10 +1,12 @@
-package life.majiang.community.model;
+package life.majiang.community.dto;
 
+import life.majiang.community.model.User;
 import lombok.Data;
 
 @Data
-public class Question {
+public class QuestionDTO {
 
+    private User user;
     private Integer id;
     private String title;
     private String description;
