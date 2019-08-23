ca727412360690d00604b0caa2b6620524d0de86
Xu Wang <xuxu.wang@mail.utoronto.ca>
2019-08-23 15:41:39 +0800

    Comment when not logged in. Will be redirected to index page to log in and this new page will be closed automatically.

--- src/main/java/life/majiang/community/exception/CustomizeErrorCode.java
+++ src/main/java/life/majiang/community/exception/CustomizeErrorCode.java
@@ -4,7 +4,7 @@ public enum CustomizeErrorCode implements ICustomizeErrorCode {
     QUESTION_NOT_FOUND(2001, "The question you are looking for cannot be found, try other ones."),
     TARGET_PARAM_NOT_FOUND(2002, "未选择任何问题或评论进行回复"),
     NO_LOGIN(2003, "当前操作需要登陆，请登录后重试"),
-    SYS_ERROR(2003, "Server got fire, maybe try again later?"),
+    SYS_ERROR(2004, "Server got fire, maybe try again later?"),
     TYPE_PARAM_WRONG(2005, "Comment type is wrong or not exist."),
     COMMENT_NOT_FOUND(2006, "The comment you are looking for cannot be found!")
     ;
