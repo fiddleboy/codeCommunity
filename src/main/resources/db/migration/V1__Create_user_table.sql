b0e014a671f25be0d45a0823de8ab210c0b4c126
Xu Wang <xuxu.wang@mail.utoronto.ca>
2019-09-03 15:01:18 +0800

    integrated flyway

--- src/main/resources/db/migration/V1__Create_user_table.sql
+++
@@ -1,9 +0,0 @@
-create table USER
-(
-    ID           INT auto_increment PRIMARY KEY NOT NULL,
-    ACCOUNT      VARCHAR(100),
-    NAME         VARCHAR(50),
-    TOKEN        CHAR(36),
-    GMT_CREATE   BIGINT,
-    GMT_MODIFIED BIGINT
-);
